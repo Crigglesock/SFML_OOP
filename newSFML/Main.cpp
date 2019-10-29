fe4160eafaa1150f2e4018573e35d1659564ce3f
Craig Dunne <A0101664@ayrshire.ac.uk>
2019-10-29 10:02:15 +0000

    Skeleton

---
+++ newSFML/Main.cpp
@@ -0,0 +1,41 @@
+#include"SFML/include/SFML/Graphics.hpp"
+
+sf::Vector2f viewSize(1024, 768);
+sf::VideoMode vm(viewSize.x, viewSize.y);
+sf::RenderWindow window(vm, "Hello SFML!!!", sf::Style::Default);
+
+int main()
+{
+	//Initialize game objects
+	//Rectangle 
+	sf::RectangleShape rect(sf::Vector2f(500.0f, 300.0f));
+	rect.setFillColor(sf::Color::Green);
+	rect.setPosition(viewSize.x / 2, viewSize.y / 2);
+	rect.setOrigin(sf::Vector2f(rect.getSize().x / 2, rect.getSize().y / 2));
+	//Circle
+	sf::CircleShape circle(100);
+	circle.setFillColor(sf::Color::Black);
+	circle.setPosition(viewSize.x / 2, viewSize.y / 2);
+	circle.setOrigin(sf::Vector2f(circle.getRadius(), circle.getRadius()));
+	//Triangle
+	sf::ConvexShape triangle;
+	triangle.setPointCount(3);
+	triangle.setPoint(0, sf::Vector2f(-100, 0));
+	triangle.setPoint(1, sf::Vector2f(0, -100));
+	triangle.setPoint(2, sf::Vector2f(100, 0));
+	triangle.setFillColor(sf::Color(65,156,156,255));
+	triangle.setPosition(viewSize.x / 2, viewSize.y / 2);
+
+	while (window.isOpen())
+	{
+		//handle keyboard events
+		//update game objects
+		window.clear(sf::Color::Red);
+		//render game objects
+		window.draw(rect);
+		window.draw(circle);
+		window.draw(triangle);
+		window.display();
+	}
+	return 0;
+}
