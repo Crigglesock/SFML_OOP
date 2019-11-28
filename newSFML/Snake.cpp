586792112856eb04cf49d73e595b770866586f57
Craig Dunne <A0101664@ayrshire.ac.uk>
2019-11-28 16:01:36 +0000

    Unpolished Segment Bug Fix

--- newSFML/Snake.cpp
+++ newSFML/Snake.cpp
@@ -18,7 +18,24 @@ void Snake::SetDirection(Direction l_dir)
 
 Direction Snake::GetDirection()
 {
-	return m_dir;
+	if (m_snakeBody.size() <= 1)
+	{
+		return Direction::None;
+	}
+
+	SnakeSegment& head = m_snakeBody[0];
+	SnakeSegment& neck = m_snakeBody[1];
+
+	if (head.position.x == neck.position.x)
+	{
+		return (head.position.y > neck.position.y ? Direction::Down : Direction::Up);
+	}
+	else if (head.position.y == neck.position.y)
+	{
+		return (head.position.x > neck.position.x ? Direction::Right : Direction::Left);
+	}
+
+	return Direction::None;
 }
 
 int Snake::GetSpeed()
